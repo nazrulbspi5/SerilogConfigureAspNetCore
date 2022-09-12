b28fe9dc4a68b3415b92b5c037c65fdd5a2fa2f6
nazrulbspi5 <nazrulbspi5@gmail.com>
2022-09-12 23:37:34 +0600

    Created new project

---
+++ SerilogAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SerilogAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
